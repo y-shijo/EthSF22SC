98dc1378c8135e9c9640e5079d0a00490b2d7451
Yoshinobu Shijo <yoshinobushijo02264@gmail.com>
2022-11-05 09:57:40 -0700

    Implement basic logic of Greeting Contract.

--- contracts/Greeting.sol
+++ contracts/Greeting.sol
@@ -2,41 +2,46 @@
 pragma solidity ^0.8.0;
 
 import "./interfaces/IGreeting.sol";
+import "./interfaces/ICampaign.sol";
 
 contract TheGreeting is 
     IGreeting
 {
-    // The user can get a list of available campaigns.
-    function getCampaignList() external view override returns (address[] memory) {
+    // List of Campaigns
+    ICampaign[] campaigns;
 
+
+    // The user can get a list of available campaigns.
+    function getCampaignList() external view override returns (ICampaign[] memory) {
+        return campaigns;
     }
 
     // The user can get a list of available words for a campaign.
     function getGreetingWordList(
-        address campaign
+        ICampaign campaign
     ) external view override returns (string[] memory) {
-
+        return campaign.getGreetingWordList();
     }
 
     // The user can select the word for a campaign.
     function selectGreetingWord(
-        address campaign,
+        ICampaign campaign,
         uint wordIndex
     ) external override {
-
+        campaign.selectGreetingWord(wordIndex);
     }
 
     // The user can get price in Wei per message for a campaign
     function getPricePerMessageInWei(
-        address campaign
+        ICampaign campaign
     ) external view override returns (uint price) {
-
+        return campaign.getPricePerMessageInWei();
     }
 
     // The user can send the greeting message to a recipient(to).
     // Make sure that the sender can only one message to a recipient.
     function send(
-        address campaign,
+        ICampaign campaign,
         address to,
         string memory messageURI
     ) external payable override {
@@ -44,31 +49,38 @@ contract TheGreeting is
 
         // TODO: Update the payment based on the sender identity.
 
+        // Send a Message via Campaign.
+        campaign.send(msg.sender, to, messageURI);
+
         // TODO: Integrate PUSH Protocol to send notification to the recipient
     }
 
     // The user can get message IDs of Campaign
     function getMessageIdsOfCampaign(
-        address campaign,
+        ICampaign campaign,
         address who,
-        ICampaign.MessageType action
+        ICampaign.MessageType messageType
     ) external view override returns (uint[] memory) {
-
+        return campaign.getMessageIds(who, messageType);
     }
 
     // The user can get message by ID
     function getMessageByIdOfCampaign(
-        address campaign,
+        ICampaign campaign,
         uint id
     ) external view override returns (ICampaign.MessageResponseDto memory) {
-
+        return campaign.getMessageById(id);
     }
 
     // The user can register new campaign
     function registerCampaign(
-        address campaign
+        ICampaign campaign_
     ) external override {
+        require(
+            ICampaign(campaign_).supportsInterface(type(ICampaign).interfaceId),
+            "Err: The given address does not comply with ICampaign."
+        );
 
+        campaigns.push(ICampaign(campaign_));
     }
-
 }
